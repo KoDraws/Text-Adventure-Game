9450d75562f0e5e400290dde75cd500c670825d1
KoDraws <76661881+KoDraws@users.noreply.github.com>
2020-12-27 15:05:48 -0800

    Style

---
+++ game.js
@@ -0,0 +1,14 @@
+function startUp() {
+	var gameData = {
+		pc:{
+			name: "",
+			height: 0,
+			muscle: 0,
+			fat: 0,
+			intelligence: 0,
+			wisdom: 0,
+			charisma: 0
+		}
+	};
+	alert("test");
+}
